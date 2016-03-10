d35c873b00f0ddeb86fdde6a613bafdbafee3a9b
Javier Laguna <lagubull@hotmail.com>
2016-03-10 14:31:07 +0000

    swift for request

---
+++ SocialChallenge/Managers/APIs/Feed/Requests/JSCFeedRequest.swift
@@ -0,0 +1,53 @@
+//
+//  JSCFeedRequest.swift
+//  SocialChallenge
+//
+//  Created by Javier Laguna on 10/03/2016.
+//
+//
+
+import Foundation
+
+/**
+HTTP GET Method.
+*/
+let kJSCHTTPRequestMethodGet = "GET" as String!
+
+@objc(JSCFeedRequest)
+
+/**
+Request to retrieve a feed.
+*/
+class JSCFeedRequest: NSMutableURLRequest {
+
+    //Mark - Retrieve
+    
+    /**
+    Creates a request for downloading the feed first page.
+    
+    :Returns: an instance of the class.
+    */
+    class func requestToRetrieveFeed () -> JSCFeedRequest {
+        
+        return self.requestToRetrieveFeedNexPageWithURL(API_END_POINT)
+    }
+    
+    /**
+    Creates a request for downloading a page of content.
+    
+    :Parameter URL - URL to download the content from.
+    
+    :Returns: an instance of the class.
+    */
+    class func requestToRetrieveFeedNexPageWithURL(URL : String) -> JSCFeedRequest {
+    
+        let request = self.init() as JSCFeedRequest
+        
+        request.HTTPMethod = kJSCHTTPRequestMethodGet
+        
+        request.URL = NSURL.init(string: URL)
+        
+        return request
+    }
+    
+}
