8cf8afed225ff1a75a598be76c901592fb5c86a2
Javier Laguna <lagubull@hotmail.com>
2016-03-15 16:27:01 +0000

    more swift classes

---
+++ SocialChallenge/NavigationController/JSCRootNavigationController.swift
@@ -0,0 +1,40 @@
+//
+//  JSCRootNavigationController.swift
+//  SocialChallenge
+//
+//  Created by Javier Laguna on 15/03/2016.
+//
+//
+
+import Foundation
+
+class JSCRootNavigationController: UINavigationController, UINavigationControllerDelegate {
+
+    var homeViewController: JSCHomeViewController?
+    
+    //MARK: Init
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init() {
+        
+        self.init(rootViewController: JSCHomeViewController.init())
+        
+        self.delegate = self
+    }
+    
+    override init(rootViewController: UIViewController) {
+        
+        super.init(rootViewController: rootViewController)
+        
+        self.homeViewController = rootViewController as? JSCHomeViewController;
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+}
---
+++ SocialChallenge/SplashViewController/JSCSplashViewController.swift
@@ -0,0 +1,50 @@
+//
+//  JSCSplashViewController.swift
+//  SocialChallenge
+//
+//  Created by Javier Laguna on 15/03/2016.
+//
+//
+
+import Foundation
+
+class JSCSplashViewController: UIViewController {
+    
+    //MARK: ViewLifeCycle
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        //TODO: show something meaningful
+        self.view.backgroundColor = .blueColor()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        //TODO: show something meaningful
+        UIView.animateWithDuration(2.0, animations: {
+            
+            self.view.backgroundColor = .whiteColor()
+            },
+            completion: { (finished: Bool) in
+                
+                self.hide()
+        })
+    }
+    
+    //MARK: Hide
+    
+    /**
+    Dissmises the view countroller
+    */
+    func hide() {
+        
+        let splashWindow = UIApplication.sharedApplication().delegate!.window as! JSCWindow
+        
+        splashWindow.hideSplashScreen()
+        
+        UIApplication.sharedApplication().statusBarHidden = false
+    }
+    
+}
